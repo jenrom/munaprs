af9aafa70488d3fb694f97e3b70d1f4e644de10d
jenrom <jenrom@gmail.com>
2011-04-13 16:29:01 +0200

    Pushing new changes

--- source/nothinbutdotnetstore/FrontController.cs
+++ source/nothinbutdotnetstore/FrontController.cs
@@ -4,9 +4,16 @@ namespace nothinbutdotnetstore
 {
     public class FrontController : IProcessIncomingWebRequests
     {
+        private readonly ICanFindCommandsThatCanProcessRequests _finder;
+
+        public FrontController(ICanFindCommandsThatCanProcessRequests finder)
+        {
+            _finder = finder;
+        }
+
         public void process(IContainRequestDetails the_request)
         {
-            throw new NotImplementedException();
+            _finder.get_the_command_that_can_handle(the_request).run_using(the_request);
         }
     }
 }
