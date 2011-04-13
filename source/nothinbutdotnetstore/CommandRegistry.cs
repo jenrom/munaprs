89614b79ad037c35a09644b74ae3d8a3aee3e6d1
jenrom <jenrom@gmail.com>
2011-04-13 16:59:40 +0200

    Pushing new changes

--- source/nothinbutdotnetstore/CommandRegistry.cs
+++ source/nothinbutdotnetstore/CommandRegistry.cs
@@ -1,12 +1,21 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace nothinbutdotnetstore
 {
     public class CommandRegistry : ICanFindCommandsThatCanProcessRequests
     {
+        private readonly IEnumerable<ICanProcessOneUniqueRequest> allCommands;
+
+        public CommandRegistry(IEnumerable<ICanProcessOneUniqueRequest> all_commands)
+        {
+            allCommands = all_commands;
+        }
+
         public ICanProcessOneUniqueRequest get_the_command_that_can_handle(IContainRequestDetails request)
         {
-            throw new NotImplementedException();
+            return allCommands.First(x => x.can_process(request));
         }
     }
 }
